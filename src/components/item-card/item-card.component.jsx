e5df4dd143005ad48e943dad0ecfb3448155ff05
josedsegurav <josed.segurav@gmail.com>
2023-04-06 00:08:05 -0500

    fix style item card, add databse, add thirdvariaton button

--- src/components/item-card/item-card.component.jsx
+++ src/components/item-card/item-card.component.jsx
@@ -6,35 +6,33 @@ import { CartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
 
 function ItemCard({ product }) {
-  
-  const { name, firstvariation, secondvariation, imageUrl } = product;
-  
+  const { name, firstvariation, secondvariation, thirdvariation, imageUrl } = product;
+
   const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = (event) => {
-    if(event.target.name === "libra") { 
+    if (event.target.name === "firstvariation") {
       addItemToCart(firstvariation);
-      
-    } else if (event.target.name === "media"){
+    } else if (event.target.name === "secondvariation") {
       addItemToCart(secondvariation);
     }
-    
   };
 
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <span className="name"> {name} </span>
-       
-      <div className="footer">
-      
-      </div>
-      <Button name="libra" className="libra" buttonType="inverted" onClick={addProductToCart} >
+
+      <div className="footer"></div>
+      <Button name="firstvariation" className="libra" buttonType="inverted" onClick={addProductToCart} >
         {firstvariation.name}
       </Button>
-      <Button name="media" buttonType="inverted" onClick={addProductToCart} >
+      <Button name="secondvariation" buttonType="inverted" onClick={addProductToCart} >
         {secondvariation.name}
       </Button>
+      {thirdvariation ? (<Button name="thirdvariation" className="third" buttonType="inverted" onClick={addProductToCart} >
+        {thirdvariation.name}
+      </Button>) : null }
     </div>
   );
 }
