8ec32d9a93a1b301963b6e8b845dcce003c3704a
josedsegurav <josed.segurav@gmail.com>
2023-06-08 00:10:15 -0500

    fix button styles con shop

--- src/components/item-card/item-card.component.jsx
+++ src/components/item-card/item-card.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import "./item-card.styles.scss";
 
@@ -9,6 +9,16 @@ function ItemCard({ product }) {
   const { name, firstvariation, secondvariation, thirdvariation, imageUrl } = product;
 
   const { addItemToCart } = useContext(CartContext);
+
+  const [buttonContainerDisplay, setButtonContainerDisplay] = useState(false);
+
+  const toggleButtonsDisplay = () => {
+    setButtonContainerDisplay(!buttonContainerDisplay);
+  };
+
+  const buttonsHide = () => {
+    setButtonContainerDisplay(false)
+  };
 
   const addProductToCart = (event) => {
     if (event.target.name === "firstvariation") {
@@ -21,22 +31,25 @@ function ItemCard({ product }) {
   };
 
   return (
-    <div className="product-card-container">
-    <div className="button-container">
-      <Button name="firstvariation" onClick={addProductToCart} >
+    <div className="product-card-container" onClick={() => toggleButtonsDisplay()}>
+    <div className="button-container" style={{display: buttonContainerDisplay && "flex", justifyContent: buttonContainerDisplay && "center"}} >
+      <Button name="firstvariation" className="first" onClick={addProductToCart} >
         {firstvariation.name}
+        <span className="price">${firstvariation.price}</span>
       </Button>
+      
       <Button name="secondvariation" className="second" onClick={addProductToCart} >
         {secondvariation.name}
+        <span className="price">${secondvariation.price}</span>
       </Button>
       {thirdvariation ? (<Button name="thirdvariation" className="third" buttonType="inverted" onClick={addProductToCart} >
-        {thirdvariation.name}
+        {thirdvariation.name} <span className="price">${thirdvariation.price}</span>
       </Button>) : null }
     
         </div>
       <img src={imageUrl} alt={`${name}`}/>
      
-      <span className="item-name"> {name} </span>
+      <span className="item-name" style={{display: buttonContainerDisplay && "none"}} > {name} </span>
       
       <div className="itemcard-footer"></div>
       
